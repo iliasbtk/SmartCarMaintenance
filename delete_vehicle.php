27251f0c277458056cd9051b3778b6417f63de99
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-14 11:20:45 +0100

    #36 Delete all maintenance, documents and services related to the deleted vehicle

--- delete_vehicle.php
+++ delete_vehicle.php
@@ -1,13 +1,23 @@
 <?php
 include('connexion.php');
 $vehicle=$_GET['vehicle'];
-$deleteVehicleReq="DELETE FROM vehicle WHERE vehicleName='$vehicle';
-	";
+
+$deleteVehicleReq="DELETE FROM vehicle WHERE vehicleName='$vehicle';";
 $sql = $link->query($deleteVehicleReq) or die("Error in the consult.." . mysqli_error($link));
 
-$deleteDocReq="DELETE FROM document WHERE vehicleName='$vehicle';
-	";
+$deleteDocReq="DELETE FROM document WHERE vehicleName='$vehicle';";
 $sql = $link->query($deleteDocReq) or die("Error in the consult.." . mysqli_error($link));
+
+$deletemaintenanceReq1="DELETE FROM maintenance WHERE vehicleName='$vehicle';";
+$sql = $link->query($deletemaintenanceReq1) or die("Error in the consult.." . mysqli_error($link));
+
+$deletemaintenanceReq2="DELETE FROM maintenancelist WHERE vehicleName='$vehicle';";
+$sql = $link->query($deletemaintenanceReq2) or die("Error in the consult.." . mysqli_error($link));
+
+$deleteServiceReq="DELETE FROM service WHERE vehicleName='$vehicle';";
+$sql = $link->query($deleteServiceReq) or die("Error in the consult.." . mysqli_error($link));
+
+
 $url="vehicles.php";
 		
 		
