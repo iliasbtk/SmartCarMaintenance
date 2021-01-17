355a69531e94c0aae67ee8fb0e741c199f1f3095
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-17 11:22:18 +0100

    #60 Retreive vehicle list from the database and append it to a dropdown list

---
+++ PHP/get_vehicles_for_maintenance.php
@@ -0,0 +1,13 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include('connexion.php');
+$username=$_GET['username'];
+$req= "SELECT * FROM vehicle WHERE username='$username';";
+$sql = $link->query($req) or die("Error in the consult.." . mysqli_error($link));
+
+echo "<option> Select a vehicle</option>";
+while($aff=mysqli_fetch_array($sql))
+{
+echo "<option value=".$aff['vehicleName'].">" . $aff['vehicleName'] . "</option>";
+}
+?>
