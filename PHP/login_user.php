60d6cba5888718d95dd6ff1b5732de04ef60c167
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-17 18:05:42 +0100

    #90 Remove password md5 encryption to ease password recover feature

--- PHP/login_user.php
+++ PHP/login_user.php
@@ -2,9 +2,9 @@
 header('Access-Control-Allow-Origin: *');
 include('connexion.php');
 $username= $_POST['username'];
-$password= md5($_POST['password']);
+$password= $_POST['password'];
 
-$req="SELECT email FROM users WHERE username='$username' AND password='$password' LIMIT 1";
+$req="SELECT email FROM users WHERE username='$username' AND password='$password' LIMIT 1;";
 $result = $link->query($req) or die("Error in the consult.." . mysqli_error($link));
 if ($result->num_rows == 1){
    while($row = mysqli_fetch_array($result)) {
