62ee7d4a090b98828ddf1a1d9a982bd47fc26edd
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-17 10:48:47 +0100

    #45 Retreive vehicle services informations from the database to display them on the dashboard

---
+++ PHP/get_services_for_dashboard.php
@@ -0,0 +1,23 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include('connexion.php');
+$username=$_GET['username'];
+
+
+$req="SELECT v.vehicleName, s.serviceName, SUM(s.cost) cumServiceCost
+			FROM vehicle v INNER JOIN service s
+			ON v.vehicleName = s.vehicleName
+			WHERE v.username = '$username'
+			GROUP BY s.serviceName";
+$sql = $link->query($req) or die("Error in the consult.." . mysqli_error($link));
+
+while($aff=mysqli_fetch_array($sql))
+{
+echo " <tr>";
+echo " <td>" . $aff['vehicleName'] . "</td>";
+echo " <td>" . $aff['serviceName'] . "</td>";
+echo " <td>" . $aff['cumServiceCost'] . "</td>";
+echo " </tr>";
+}
+?>
+
