0d48ed0872bbf4fb0180feef6eaa668dffde6a82
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-17 10:38:33 +0100

    #41 Get user information from the database.

---
+++ PHP/login_user.php
@@ -0,0 +1,18 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+include('connexion.php');
+$username= $_POST['username'];
+$password= md5($_POST['password']);
+
+$req="SELECT email FROM users WHERE username='$username' AND password='$password' LIMIT 1";
+$result = $link->query($req) or die("Error in the consult.." . mysqli_error($link));
+if ($result->num_rows == 1){
+   while($row = mysqli_fetch_array($result)) {
+      echo $row['email'];
+   }  
+}  
+else {
+   echo 'Login failed';
+     
+}
+?>
