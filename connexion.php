230fae1668fd34420c6c119252569a92abd03bce
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-17 12:00:02 +0100

    #78 Remove old files as explained in commit #39

--- connexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-$nomserveur='localhost';//nom du serveur
-$nomdb='SmartCarMaintenance'; //nom de la base de donné
-$login='root';//login de l'utilisateur
-$pass='';//mot de pass
-$link = mysqli_connect($nomserveur,$login,$pass,$nomdb) or die("Error " . mysqli_error($link)); 
-$result = $link->query("SET NAMES UTF8")
-?>
