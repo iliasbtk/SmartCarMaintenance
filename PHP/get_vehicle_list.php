300f477e043a04a8acc9ee81628a36de4edaaba4
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-19 00:19:29 +0100

    #121 Update of get vehicle list

--- PHP/get_vehicle_list.php
+++ PHP/get_vehicle_list.php
@@ -7,10 +7,10 @@ $sql = $link->query($req) or die("Error in the consult.." . mysqli_error($link))
 
 while($aff=mysqli_fetch_array($sql))
 {
-echo " <tr>";
-echo " <td>" . $aff['vehicleName'] . "</td>";
-echo " <td><input type='number' id=input_" . $aff['idV'] . " value =" . $aff['currentMileage'] . "></td>";
-echo " <td><button class='ui-btn' onclick='update_vehicle(this.id);' id=".$aff['idV']." >update</button> </td>";
-echo " </tr>";
+echo " <div>";
+echo " <label for=input_" . $aff['idV'] . ">" . $aff['vehicleName'] . "</label>";
+echo " <input type='number' id=input_" . $aff['idV'] . " value =" . $aff['currentMileage'] . ">";
+echo " <button class='ui-btn' onclick='update_vehicle(this.id);' id=".$aff['idV']." >update</button>";
+echo " </div>";
 }
 ?>
