dcb605c16975b7e26ed56e044fc1de2cf3fc4cb9
iliasbtk <ilias.betkaoui@gmail.com>
2021-01-18 17:51:32 +0100

    #113 Bug fix - Default selecetd vehicle in the dropdown lists

--- PHP/get_vehicles_for_maintenance.php
+++ PHP/get_vehicles_for_maintenance.php
@@ -5,7 +5,7 @@ $username=$_GET['username'];
 $req= "SELECT * FROM vehicle WHERE username='$username';";
 $sql = $link->query($req) or die("Error in the consult.." . mysqli_error($link));
 
-echo "<option> Select a vehicle</option>";
+echo "<option></option>";
 while($aff=mysqli_fetch_array($sql))
 {
 echo "<option value=".$aff['vehicleName'].">" . $aff['vehicleName'] . "</option>";
